b764976fc6d82a72ef13d46a2c0e13a60ebe5216
GreenDoug <jadenr211@gmail.com>
2026-03-14 12:11:35 +0800

    upd

--- backend/api/control_panel/_require_superadmin.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../../config/auth.php';
-requireRole(['super admin', 'admin', 'coach', 'employee']);
-
-function currentUserId(): int
-{
-    return (int)($_SESSION['user']['id'] ?? 0);
-}
